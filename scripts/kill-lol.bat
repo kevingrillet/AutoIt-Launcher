1a1696eb6b4565dc75ea272c668da0bd0f5cf95e
kevingrillet <kevin.grillet@gmail.com>
2022-07-26 17:24:05 +0200

    Blitz -> Porofessor

---
+++ scripts/kill-lol.bat
@@ -0,0 +1,23 @@
+:: RiotClient
+taskkill /F /IM RiotClientCrashHandler.exe
+taskkill /F /IM RiotClientServices.exe
+
+:: League Game
+taskkill /F /IM LeagueCrashHandler.exe
+taskkill /F /IM "League of Legends.exe"
+
+:: LeagueClient
+taskkill /F /IM LeagueClient.exe
+taskkill /F /IM LeagueClientUx.exe
+taskkill /F /IM LeagueClientUxRender.exe
+
+:: Blitz
+taskkill /F /IM Blitz.exe
+
+:: Porofessor
+taskkill /F /IM OverwolfHelper64.exe
+taskkill /F /IM OverwolfHelper.exe
+taskkill /F /IM OverwolfBrowser.exe
+taskkill /F /IM Overwolf.exe
+
+pause
